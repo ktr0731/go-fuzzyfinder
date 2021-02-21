7ca70305b0c022c0d9e81ee660a954f4bcabe20d
tjmtmmnk <tsujimoto027@gmail.com>
2021-02-21 10:19:41 +0900

    Revert "revert the chages which migrates terminal library from nsf/termbox-go to gdamore/tcell (#47)"
    This reverts commit f3bcd190b06c59d1a8c2fe16e982444363886f35.
    
    # Conflicts:
    #	go.mod
    #	go.sum

--- helper_test.go
+++ helper_test.go
@@ -1,16 +1,13 @@
 package fuzzyfinder
 
-import "time"
-
 func New() *finder {
 	return &finder{}
 }
 
 func NewWithMockedTerminal() (*finder, *TerminalMock) {
 	f := New()
-	m := f.UseMockedTerminal()
+	m := f.UseMockedTerminalV2()
 	w, h := 60, 10 // A normally value.
 	m.SetSize(w, h)
-	m.sleepDuration = 500 * time.Microsecond
 	return f, m
 }
