3fab5970976091ac2548ec5216a577105fd89c10
ktr0731 <ktr@syfm.me>
2021-08-31 16:22:18 +0900

    update project config

--- tools/tools.go
+++ tools/tools.go
@@ -1,9 +1,9 @@
+//go:build tools
 // +build tools
 
 package tools
 
 import (
 	_ "github.com/Songmu/gocredits/cmd/gocredits"
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/goreleaser/goreleaser"
 )
